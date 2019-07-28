d1e0dc5b0535ff7258969ffd552f7355cb916c77
taegyung <taegyung@sogang.ac.kr>
2019-07-29 04:12:25 +0900

    complete mysql installation script

--- install-mysql.sh
+++ install-mysql.sh
@@ -3,14 +3,14 @@
 sudo apt-get install libaio1
 ## Download generic binaries from Oracle.
 wget https://dev.mysql.com/get/Downloads/MySQL-5.6/mysql-5.6.45-linux-glibc2.12-x86_64.tar.gz
-tar -xf mysql-5.6.44.tar.gz
+tar -xf mysql-5.6.45-linux-glibc2.12-x86_64.tar.gz
 
 # Follow the instructions of: https://dev.mysql.com/doc/refman/5.6/en/binary-installation.html
 sudo groupadd mysql
 sudo useradd -r -g mysql -s /bin/false mysql
-tar zxf mysql-5.6.45-linux-glibc2.12-x86_64.tar.gz
 rm mysql-5.6.45-linux-glibc2.12-x86_64.tar.gz
 sudo ln -s `pwd`/mysql-5.6.45-linux-glibc2.12-x86_64 /usr/local/mysql
-sudo /usr/local/mysql/scripts/mysql_install_db --user=mysql --basedir /usr/local/mysql
-# sudo /usr/local/bin/mysqld_safe &
-sudo /usr/local/mysql/bin/mysql_secure_installation
+export PATH=$PATH:/usr/local/mysql/bin
+sudo /usr/local/mysql/scripts/mysql_install_db --user=mysql --basedir /usr/local/mysql --datadir=/usr/local/mysql/data
+sudo /usr/local/mysql/bin/mysqld_safe &
+/usr/local/mysql/bin/mysql_secure_installation
