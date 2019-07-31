c1744e1a59c45ea0118fe7656f14853b2116468a
taegyung <taegyung@sogang.ac.kr>
2019-08-01 06:29:39 +0900

    make index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :tvs
   resources :movies
+
+  get 'index/index'
+  root 'index#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
